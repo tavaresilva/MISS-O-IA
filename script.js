6094f823dff66cc1c6103ed7cc43c3ba53effa66
tavaresilva <137805372+tavaresilva@users.noreply.github.com>
2025-05-13 07:43:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
