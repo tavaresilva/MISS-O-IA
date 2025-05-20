20f8e51d6fb392063c7711422a5feda9aa954bcb
tavaresilva <137805372+tavaresilva@users.noreply.github.com>
2025-05-20 07:36:33 -0300

    f

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
   {
     enunciado:
-      "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-    alternativas: ["Isso é assustador!", "Isso é maravilhoso!"],
+      "10+10",
+    alternativas: ["20", "2"],
   },
   {
     enunciado:
