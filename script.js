077a9827806fa86656f7152d1884c8b9b8c60b5c
tavaresilva <137805372+tavaresilva@users.noreply.github.com>
2025-05-27 08:28:41 -0300

    jj

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function mostraPergunta() {
   caixaPerguntas.textContent = perguntaAtual.enunciado;
   mostraAlternativas();
 }
-function mostraAlternativas() {}
-
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
 mostraPergunta();
